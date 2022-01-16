704d10984c5cc1c98c649001fe5447172d1466af
Aleksandr Shalagin <ssanchesko@gmail.com>
2022-01-16 16:06:24 +0500

    Reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,5 +1,5 @@
 public class CreditPaymentService {
-    public double calculate (int amount, double creditRate, int months) { // Сумма кредита, процентная ставка, месяцы
+    public double calculate(int amount, double creditRate, int months) { // Сумма кредита, процентная ставка, месяцы
         /* Формула аннуитетного платежа:
         P = S * (r * (1+r)^n) / (1+r)^n - 1
         P - аннуитетный платеж;
